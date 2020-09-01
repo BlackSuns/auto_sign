e8df1a159cec87179ab4d7f35f0f5819871be862
fishjohn <1258676867@qq.com>
2020-09-01 20:14:31 +0800

    Initial commit

---
+++ my_daka.py
@@ -0,0 +1,135 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/8/30 13:39
+# @Author : luohx
+# @File : my_daka.py
+# @Software: PyCharm
+
+import time
+import pytz
+import smtplib
+import json
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from email.header import Header
+from email.mime.text import MIMEText
+
+from verification_code import VerificationCode
+
+
+class DaKa:
+    def __init__(self, reg_url, user_info):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+
+        self.user_info = user_info
+        self.reg_url = reg_url
+        # self.driver = webdriver.Chrome(options=chrome_options)  # 窗口不弹出
+        self.driver = webdriver.Chrome()
+
+    def register(self):
+        register_result = False
+        self.driver.get(self.reg_url)  # 打开登录界面，需要关掉电脑的代理设置
+        time.sleep(2)
+
+        self.driver.save_screenshot('pictures.png')
+        ver_code_pos = (1388, 291, 1505, 333)  # 验证码位置(left,top,right,bottom)
+        ver_code = VerificationCode('pictures.png', ver_code_pos)
+        uid = self.user_info['uid']
+        pwd = self.user_info['pwd']
+        vercode = ver_code.image_str()
+        if vercode is None:
+            return register_result
+
+        self.driver.find_element_by_xpath("//input[@placeholder='请输入学号']").send_keys(uid)
+        self.driver.find_element_by_xpath("//input[@placeholder='请输入密码']").send_keys(pwd)
+        self.driver.find_element_by_xpath("//input[@placeholder='请输入验证码']").send_keys(vercode)
+
+        last_url = self.driver.current_url
+        self.driver.find_element_by_xpath("//button[1]").click()  # 登录
+        time.sleep(3)
+
+        # 判断网页是否跳转
+        if self.driver.current_url != last_url:
+            register_result = True
+            self.driver.switch_to.window(self.driver.window_handles[0])  # 跳转页面
+        else:
+            register_result = False
+        return register_result
+
+    def daka(self):
+        self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[2]/a").click()
+        time.sleep(3)
+        self.driver.switch_to.window(self.driver.window_handles[0])  # 跳转页面
+        t = int(datetime.fromtimestamp(int(time.time()),
+                                       pytz.timezone('Asia/Shanghai')).strftime('%H'))
+        if 7 <= t < 9:
+            self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[3]/button").click()
+            self.send_email("早晨体温打卡成功")
+        if 12 <= t < 14:
+            self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[5]/button").click()
+            self.send_email("中午体温打卡成功")
+        if 20 <= t < 22:
+            self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[7]/button").click()
+            self.send_email("晚上体温打卡成功")
+            result = True
+        else:
+            print("未到打卡时间")
+            self.send_email("发生错误，未到打卡时间打卡")
+            result = False
+        self.driver.close()  # 关闭网页
+        return result
+
+    def send_email(self, msg):
+        my_sender = '**********'  # 发件人邮箱账号
+        my_user = self.user_info['mail']  # 收件人邮箱账号
+        qq_code = '**********'
+        smtp_server = 'smtp.qq.com'
+        smtp_port = 465
+
+        stmp = smtplib.SMTP_SSL(smtp_server, smtp_port)
+        stmp.login(my_sender, qq_code)
+
+        message = MIMEText(msg, 'plain', 'utf-8')  # 发送内容
+        message['From'] = Header("无情的打卡机器人", 'utf-8')  # 发件人
+        message['To'] = Header("管理员", 'utf-8')  # 收件人
+        subject = '体温打卡汇报'
+        message['Subject'] = Header(subject, 'utf-8')  # 邮件标题
+
+        try:
+            stmp.sendmail(my_sender, my_user, message.as_string())
+        except Exception as e:
+            print('邮件发送失败--' + str(e))
+        print('邮件发送成功')
+
+    def close(self):
+        self.driver.close()  # 关闭网页
+
+
+def main():
+    register_url = r'http://xsgl.gdut.edu.cn/index.php/mobile/student/task-temperature?version-checking-redirection=8' \
+                   r'.473923891538044 '
+
+    file = open("user_info.json", encoding="utf-8")
+    info = json.load(file)
+    user_info = info['useraccount']
+
+    for each_user in user_info:
+        times = 10
+        result = False
+        while times >= 0:
+            my_daka = DaKa(register_url, each_user)
+            if my_daka.register():
+                result = my_daka.daka()
+                break
+            else:
+                my_daka.close()
+            times = times - 1
+        if result:
+            print(each_user['username'] + "打卡成功")
+        else:
+            print(each_user['username'] + "打卡失败")
+
+
+if __name__ == '__main__':
+    main()
---
+++ verification_code.py
@@ -0,0 +1,122 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/8/30 14:22
+# @Author : luohx
+# @File : verification_code.py
+# @Software: PyCharm
+
+
+import time
+import re
+import pytesseract
+from PIL import Image  # 用于打开图片和对图片处理
+
+
+def str_split(image):
+    inletter = False  # 找出每个字母开始位置
+    foundletter = False  # 找出每个字母结束位置
+    start = 0
+    letters = []  # 存储坐标
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            pix = image.getpixel((x, y))
+            if pix != 255:
+                inletter = True
+        if foundletter == False and inletter == True:
+            foundletter = True
+            start = x
+        if foundletter == True and inletter == False:
+            foundletter = False
+            end = x
+            letters.append((start, end))
+        inletter = False
+
+    # 因为切割出来的图像有可能是噪声点
+    # 筛选可能切割出来的噪声点,只保留开始结束位置差值最大的位置信息
+    subtract_array = []  # 存储 结束-开始 值
+    for each in letters:
+        subtract_array.append(each[1] - each[0])
+    reSet = sorted(subtract_array, key=lambda x: x, reverse=True)[0:4]
+    letter_choice = []  # 存储 最终选择的点坐标
+    for each in letters:
+        if int(each[1] - each[0]) in reSet:
+            letter_choice.append(each)
+
+    image_split_array = []  # 存储切割后的图像
+    for letter in letter_choice:
+        im_split = image.crop((letter[0], 0, letter[1], image.size[1]))  # (切割的起始横坐标，起始纵坐标，切割的宽度，切割的高度)
+        im_split = im_split.resize((13, 42))  # 转换格式
+        image_split_array.append(im_split)
+
+    return image_split_array[0:4]
+
+
+class VerificationCode:
+    def __init__(self, picture_name, ver_code_pos=None):
+        if ver_code_pos is None:
+            ver_code_pos = (0, 0, 0, 0)
+        self.picture_name = picture_name
+        self.ver_code_pos = ver_code_pos
+
+    def get_pictures(self):
+        page_snap_obj = Image.open(self.picture_name)
+        time.sleep(1)
+
+        image_obj = page_snap_obj.crop(self.ver_code_pos)
+        return image_obj
+
+    def process_pictures(self):
+        image_obj = self.get_pictures()
+        img = image_obj.convert("L")  # 转灰度
+
+        pixdata = img.load()
+        w, h = img.size
+        threshold = 60
+        # 遍历所有像素，大于阈值的为黑色
+        for y in range(h):
+            for x in range(w):
+                if pixdata[x, y] < threshold:
+                    pixdata[x, y] = 255  # 黑
+                else:
+                    pixdata[x, y] = 0
+        return img  # 返回一个二值灰度图像
+
+    def delete_spot(self):
+        images = self.process_pictures()
+        data = images.getdata()
+        w, h = images.size
+        black_point = 0
+        for x in range(1, w - 1):
+            for y in range(1, h - 1):
+                mid_pixel = data[w * y + x]  # 中央像素点像素值
+                if mid_pixel < 50:  # 找出上下左右四个方向像素点像素值
+                    top_pixel = data[w * (y - 1) + x]
+                    left_pixel = data[w * y + (x - 1)]
+                    down_pixel = data[w * (y + 1) + x]
+                    right_pixel = data[w * y + (x + 1)]
+                    # 判断上下左右的黑色像素点总个数
+                    if top_pixel < 10:
+                        black_point += 1
+                    if left_pixel < 10:
+                        black_point += 1
+                    if down_pixel < 10:
+                        black_point += 1
+                    if right_pixel < 10:
+                        black_point += 1
+                    if black_point < 1:
+                        images.putpixel((x, y), 255)
+                    black_point = 0
+        return images
+
+    def image_str(self):
+        image = self.delete_spot()
+        result_array = []
+        image_split_array = str_split(image)
+        pytesseract.pytesseract.tesseract_cmd = r"E:\tesseract\Tesseract-OCR\tesseract.exe"
+        for each in image_split_array:
+            result = pytesseract.image_to_string(each, config="-psm 10")  # 图片转文字
+            result = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", result)  # 去除识别出来的特殊字符
+            result_array.append(result)
+        for each in result_array:
+            if each == "":
+                return None
+        return ''.join(result_array)
